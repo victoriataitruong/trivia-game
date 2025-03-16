96d56835020017e834c2a4bef474a69b29302a18
Victoria Truong <victoriataitruong@gmail.com>
2025-03-16 16:11:26 -0700

    updated total points

--- app/components/Scoreboard.js
+++ app/components/Scoreboard.js
@@ -2,7 +2,7 @@
 export default function Scoreboard({ score }) {
   return (
     <div className="mt-6 text-white bg-green-500 px-4 py-2 rounded-lg shadow-lg">
-      <h2 className="text-xl font-bold">Questions correct so far: {score}</h2>
+      <h2 className="text-xl font-bold">Total Points Earned: {score}</h2>
     </div>
   );
 }
